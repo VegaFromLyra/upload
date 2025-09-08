785d69ee3fe13d498d2423093b0ce2a8aea37f1c
Asha Balasubramaniam <asha.ab@gmail.com>
2025-09-08 12:41:21 -0400

    Add basic file picker click handling

--- app/javascript/controllers/files_controller.js
+++ app/javascript/controllers/files_controller.js
@@ -1,7 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["fileInput"]
+
+  upload() {
+    console.log("Select a file to upload")
+    this.fileInputTarget.click()
+  }
+
   connect() {
-    console.log("Hello from Files Controller")
+    this.fileInputTarget.addEventListener('change', this.handleFileSelect.bind(this)) 
+  }
+
+  handleFileSelect(event) {
+    const files = event.target.files
+
+    console.log("Files selected:", files)
   }
 }

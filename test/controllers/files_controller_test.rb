2a23fd9e8b6b8fd0fed9018fcdfc29bbb31560f2
Asha Balasubramaniam <asha.ab@gmail.com>
2025-09-28 17:05:50 -0400

    Fix url helper

--- test/controllers/files_controller_test.rb
+++ test/controllers/files_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class FilesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get files_index_url
+    get files_url
     assert_response :success
   end
 end
